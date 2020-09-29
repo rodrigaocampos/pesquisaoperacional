32424d16f4fbbf619d54141f0f3a69d1b91f4569
rodrigo campos <rodrigodeveloper96@gmail.com>
2020-09-29 18:42:27 -0300

    correção de erros da questão 5

--- Atividade revisão 1/exercicio5.py
+++ Atividade revisão 1/exercicio5.py
@@ -1,5 +1,33 @@
-manipulador = open('arquivo.txt', 'r')
-print('metodo read')
-print(manipulador.read())
+def somarMatrizes(matriz, matriz2):
+    if(len(matriz) != len(matriz2) or len(matriz[0]) != len(matriz2[0])):
+        return None
+    resultado = []
+    for i in range(len(matriz)):   
+        resultado.append([])
+        for j in range(len(matriz[0])):
+            soma = int(matriz[i][j]) + int(matriz2[i][j])
+            resultado[i].append(soma)
+    return resultado
 
-manipulador.seek(0)
+arq = open('arquivo.txt', 'r')  
+texto = []  
+matriz = [] 
+matriz2 = []
+texto = arq.readlines() 
+
+for i in range(len(texto)):        
+    if(i < 10):
+        matriz.append(texto[i].split())
+    else:
+        if(texto[i] != '\n'):
+            matriz2.append(texto[i].split())
+        
+soma = somarMatrizes(matriz, matriz2) 
+print('==================================================')
+print("Resultado da soma")
+if soma is not None:
+    for i in soma:
+        print(i)
+else:
+    print('número colunas ou Linhas diferentes')
+arq.close()
---
+++ Atividade revisão 1/readme.txt
@@ -0,0 +1,3 @@
+os scripts foram criados em python3 em ambiente linux, para executar:
+
+python3 + nome_do_arquivo.py
